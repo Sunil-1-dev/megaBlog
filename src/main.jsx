a600441c3c758d1e0cc6010f52a9e7a42e5e7b1e
Sunil-2056 <khadkasunil107@gmail.com>
2025-07-01 15:47:51 +0545

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -82,7 +82,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+    <RouterProvider router={router}/>
     </Provider>
   </StrictMode>,
 )
